3e41ddd2323f71af28ed85f83969773c874e401d
pierre tardif <pierre.tardif00@gmail.com>
2020-04-09 22:12:13 +0100

    added a readme in example-Matte

--- src/lib/ARBodyTrackingBool.h
+++ src/lib/ARBodyTrackingBool.h
@@ -5,7 +5,7 @@
 //  Created by Pierre Tardif on 09/04/2020.
 //
 
-// Comment those to remove depth Body Tracking
+// Comment those to add/remove depth Body Tracking
 #ifndef ARBodyTrackingBool_h
-#define ARBodyTrackingBool_h
+//#define ARBodyTrackingBool_h
 #endif /* ARBodyTrackingBool_h */
